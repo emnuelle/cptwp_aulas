2c8340aa62c633bf405327115102dfca7f8efef3
Emanuelle Soares <126582921+emnuelle@users.noreply.github.com>
2023-03-06 19:21:41 -0300

    Add files via upload
    >>> Todas as atividades completas

---
+++ pg008.py
@@ -0,0 +1,11 @@
+kor = 8
+print( 8, "x", 1, "=", kor * 1)
+print( 8, "x", 2, "=", kor * 2)
+print( 8, "x", 3, "=", kor * 3)
+print( 8, "x", 4, "=", kor * 4)
+print( 8, "x", 5, "=", kor * 5)
+print( 8, "x", 6, "=", kor * 6)
+print( 8, "x", 7, "=", kor * 7)
+print( 8, "x", 8, "=", kor * 8)
+print( 8, "x", 9, "=", kor * 9)
+print( 8, "x", 10, "=", kor * 10)
